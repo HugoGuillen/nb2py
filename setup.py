2b92ceb78f58af2cee97361eb8466a24511b79e7
hugo <hugoagr@gmail.com>
2017-07-16 15:12:44 -0700

    pypi setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name='nb2py',
-      version='0.1',
-      description='Dumps marked code cells from a Jupyter notebook into a text file.',
-      url='http://github.com/HugoGuillen/nb2py',
-      author='Hugo Guillen-Ramirez',
-      author_email='hugoagr@gmail.com',
+setup(name = 'nb2py',
+      packages = ['nb2py'],
+      version = '0.1',
+      description = 'Dumps marked code cells from a Jupyter notebook into a text file.',
+      author = 'Hugo Guillen-Ramirez',
+      author_email = 'hugoagr@gmail.com',
+      url = 'https://github.com/HugoGuillen/nb2py',
+      download_url = 'https://github.com/HugoGuillen/nb2py/archive/0.1.tar.gz',      
       license='MIT',
-      packages=['nb2py'],
+      keywords = ['jupyter'],
       zip_safe=False)
