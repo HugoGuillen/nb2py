50890fa3f8be6f2709994fee6642f4740966060c
hugo <hugoagr@gmail.com>
2017-07-16 15:02:57 -0700

    project re-structured

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nb2py',
+      version='0.1',
+      description='Dumps marked code cells from a Jupyter notebook into a text file.',
+      url='http://github.com/HugoGuillen/nb2py',
+      author='Hugo Guillen-Ramirez',
+      author_email='hugoagr@gmail.com',
+      license='MIT',
+      packages=['nb2py'],
+      zip_safe=False)
