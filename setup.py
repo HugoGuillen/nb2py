576498683c7260f7c9d8b35e19636767d8463a03
hugo <hugoagr@gmail.com>
2017-07-16 16:52:40 -0700

    setup for 0.1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(name = 'nb2py',
       packages = ['nb2py'],
-      version = '0.1.1',
+      version = '0.1.3',
       description = 'Dumps marked code cells from a Jupyter notebook into a text file.',
       author = 'Hugo Guillen-Ramirez',
       author_email = 'hugoagr@gmail.com',
       url = 'https://github.com/HugoGuillen/nb2py',
-      download_url = 'https://github.com/HugoGuillen/nb2py/archive/0.1.1.tar.gz',      
+      download_url = 'https://github.com/HugoGuillen/nb2py/archive/0.1.3.tar.gz',      
       license='MIT',
       keywords = ['jupyter'],
       zip_safe=False)
