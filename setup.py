5034b589112cc0555e9fdfa010bd1adb910c08d0
hugo <hugoagr@gmail.com>
2017-07-16 17:14:12 -0700

    0.1.4

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup
+import subprocess
+import io
 
+subprocess.check_output("pandoc --from=markdown --to=rst --output=README.rst README.md",
+                        stderr=subprocess.STDOUT,
+                        shell=True)
+
+with io.open('README.rst', encoding="utf-8") as f:
+    long_description = f.read()
+    
 setup(name = 'nb2py',
       packages = ['nb2py'],
-      version = '0.1.3',
+      version = '0.1.4',
       description = 'Dumps marked code cells from a Jupyter notebook into a text file.',
+      long_description=long_description,
       author = 'Hugo Guillen-Ramirez',
       author_email = 'hugoagr@gmail.com',
       url = 'https://github.com/HugoGuillen/nb2py',
-      download_url = 'https://github.com/HugoGuillen/nb2py/archive/0.1.3.tar.gz',      
+      download_url = 'https://github.com/HugoGuillen/nb2py/archive/0.1.4.tar.gz',
       license='MIT',
       keywords = ['jupyter'],
       zip_safe=False)
